b77b129bec5acb01a15fd5f5a44aa0ad301af50c
Franco Peralta <franco.peralta@upch.pe>
2026-08-31 19:08:10 -0500

    Para simular la inclusión

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker7
-#SBATCH --output="CAPS_in_w1_densitymapInc.out"
+#SBATCH --output="CAPS_in_w1_simulateInc.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "generateDensityMap_Inc;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "simulateInc9_CAPS_steering_freq;  exit"
