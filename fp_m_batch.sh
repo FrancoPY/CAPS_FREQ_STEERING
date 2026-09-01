db396b6a25ae0bcf37848d299cf85f96d4f964a6
Franco Peralta <franco.peralta@upch.pe>
2026-08-31 19:05:20 -0500

    Actualización de frecuencias en el código de inclusión y del sbatch

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
-#SBATCH --partition=gamerpcs
-#SBATCH --nodelist=worker3
+#SBATCH --partition=thinkstation
+#SBATCH --nodelist=worker7
 #SBATCH --output="CAPS_in_w1_densitymapInc.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "simulateInc9_CAPS_steering_freq;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "generateDensityMap_Inc;  exit"
