8b2f2c89b4d8fce5f94fdecf59385b6aefe79a2a
Franco Peralta <franco.peralta@upch.pe>
2026-09-01 19:17:58 -0500

    Para simular el simulateBgnd

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
-#SBATCH --nodelist=worker7
-#SBATCH --output="CAPS_in_w1_simulateInc.out"
+#SBATCH --nodelist=worker10
+#SBATCH --output="CAPS_in_w1_simulateBgnd.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "simulateInc9_CAPS_steering_freq;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "simulateBgnd6_CAPS_steering_freq;  exit"
