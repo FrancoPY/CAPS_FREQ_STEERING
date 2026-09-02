9bf12dfe9f85c1220c92cf228074a62c3db65006
Franco Peralta <franco.peralta@upch.pe>
2026-09-02 01:52:55 -0500

    .

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker7
-#SBATCH --output="CAPS_in_w1_simulateBgnd.out"
+#SBATCH --output="CAPS_in_w1_generatedensitymapBgnd.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "generateDensityMap_Bgnd";  exit"
