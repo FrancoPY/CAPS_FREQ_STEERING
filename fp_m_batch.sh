dbb854d885e7081a54b1cbed6b60b7e6d803f5e4
Franco Peralta <franco.peralta@upch.pe>
2026-09-04 12:56:56 -0500

    Cálculo de DM_ángulo_15

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker7
-#SBATCH --output="CAPS_in_w1_Beamforming.out"
+#SBATCH --output="CAPS_in_w1_ComputeB/A.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "beamform_to_bf;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL;  exit"
