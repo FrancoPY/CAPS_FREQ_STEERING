219db36cd164d71f78e40d337484fc767a15b7b5
Franco Peralta <franco.peralta@upch.pe>
2026-09-04 18:42:00 -0500

    Frecuencia 5MHz angulo de -5

--- BA_COMPUTE_DM_FINAL.m
+++ BA_COMPUTE_DM_FINAL.m
@@ -1,12 +1,12 @@
 refSample = 'Phantom_bgnd_BA6_CAPS_FreqSteering_v1';
 samSample = 'Phantom_inc_BA11_CAPS_FreqSteering_v1';
-freq = 7e6;
+freq = 5e6;
 
 
 probe = 'L14-5u';
 basedir = fullfile(pwd,'FRECUENCIA-STEERING');
 freqStr = sprintf('%dMHz',freq/1e6);
-angleStr = 'Angle_10';
+angleStr = 'Angle_-5';
 
 refDir = fullfile(basedir,refSample, probe, freqStr, 'bf', angleStr);
 samDir = fullfile(basedir,samSample, probe, freqStr, 'bf', angleStr);
