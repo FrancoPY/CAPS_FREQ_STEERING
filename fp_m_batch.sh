6d6bf9637749052eac414b1f54fc94638a9c1537
Franco Peralta <franco.peralta@upch.pe>
2026-09-05 16:52:48 -0500

    .

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
-#SBATCH --nodelist=worker10
-#SBATCH --output="CAPS_in_w2_generateDensityMapBgnd.out"
+#SBATCH --nodelist=worker7
+#SBATCH --output="CAPS_in_w2_simulateBgnd.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "simulateBgnd6_CAPS_steering_freq;  exit"
