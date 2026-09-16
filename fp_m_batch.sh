c811327f68cf777c2827a859c9c7f9251b3d8cf2
Franco Peralta <franco.peralta@upch.pe>
2026-09-16 16:02:24 -0500

    simulations  BGND w3

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -5,4 +5,4 @@
 #SBATCH --nodelist=worker8
 #SBATCH --output="CAPS_in_w3_simuateBgnd.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "simuLateBgnd6_CAPS_steering_freq;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "simulateBgnd6_CAPS_steering_freq;  exit"
