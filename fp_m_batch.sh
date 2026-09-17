f57ba91fa6a258a10dc2fdce8523be6d2b469f07
Franco Peralta <franco.peralta@upch.pe>
2026-09-17 08:39:01 -0500

    BA compute w3

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker10
-#SBATCH --output="CAPS_in_w3_beamforming.out"
+#SBATCH --output="CAPS_in_w3_BAcompute.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "beamform_to_bf;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL;  exit"
