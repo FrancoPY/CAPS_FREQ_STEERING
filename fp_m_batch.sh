72f6b2cad51e2565769393b8fca174d2d5e3b946
Franco Peralta <franco.peralta@upch.pe>
2026-09-22 12:15:04 -0500

    Nueva simulación consd 0.05

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -5,4 +5,4 @@
 #SBATCH --nodelist=worker7
 #SBATCH --output="CAPS_in_w3.1_BA_COMPUTE.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL_SIN_PROMEDIAR;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL;  exit"
