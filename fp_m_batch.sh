830d0a3fd152f91d744456a814e5e24c338b85b2
Franco Peralta <franco.peralta@upch.pe>
2026-09-22 18:50:40 -0500

    Nueva simulación consd 0.05

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -5,4 +5,4 @@
 #SBATCH --nodelist=worker7
 #SBATCH --output="CAPS_in_w3.1_BA_COMPUTE.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "generateDensityMap;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "simulateBgnd6_CAPS_steering_freq;  exit"
