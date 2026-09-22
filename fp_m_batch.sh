4efb3bbd80e1f93757fd12e302465b1c4062bdc8
Franco Peralta <franco.peralta@upch.pe>
2026-09-21 20:50:00 -0500

    Nueva simulación consd 0.05

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker7
-#SBATCH --output="CAPS_in_w3_BAcompute.out"
+#SBATCH --output="CAPS_in_w3.1_Simulate.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL_SIN_PROMEDIAR;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "generateDensityMap;  exit"
