565b583600eb9b1700ab2f5acaa054d4b5e05156
Franco Peralta <franco.peralta@upch.pe>
2026-09-24 20:07:26 -0500

    BA compute w3_6MHz

--- fp_m_batch.sh
+++ fp_m_batch.sh
@@ -3,6 +3,6 @@
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
 #SBATCH --nodelist=worker9
-#SBATCH --output="CAPS_in_w3_correction_computeBA.out"
+#SBATCH --output="CAPS_in_w3_correction_computeBA_solo.out"
 
-srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL;  exit"
+srun matlab -nosplash -nodesktop -nodisplay -r "BA_COMPUTE_DM_FINAL_SIN_PROMEDIAR;  exit"
